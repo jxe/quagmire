a2995bfa69e6293638bf673471d746822065f56d
Joe Edelman <joe.edelman@gmail.com>
2026-06-25 11:00:00 +0200

    Fix inline code chip backgrounds

--- Sources/Editor/Text/InlineCodeChipRenderer.swift
+++ Sources/Editor/Text/InlineCodeChipRenderer.swift
@@ -14,11 +14,7 @@ struct InlineCodeChipRenderer: TextRenderer {
 
     func draw(layout: Text.Layout, in context: inout GraphicsContext) {
         for line in layout {
-            for run in line where run[InlineCodeChipAttribute.self] != nil {
-                let rect = run.typographicBounds.rect.insetBy(
-                    dx: 0,
-                    dy: -NotionStyle.inlineCodeVerticalPadding
-                )
+            for rect in inlineCodeRects(in: line) {
                 context.fill(
                     Path(roundedRect: rect, cornerRadius: NotionStyle.inlineCodeRadius),
                     with: .color(NotionStyle.codeBackground)
@@ -28,6 +24,36 @@ struct InlineCodeChipRenderer: TextRenderer {
 
         for line in layout {
             context.draw(line)
+        }
+    }
+
+    private func inlineCodeRects(in line: Text.Layout.Line) -> [CGRect] {
+        let sourceRects = line.compactMap { run -> CGRect? in
+            guard run[InlineCodeChipAttribute.self] != nil else { return nil }
+            return run.typographicBounds.rect
+        }
+        guard !sourceRects.isEmpty else { return [] }
+
+        let mergedRects = sourceRects
+            .sorted { $0.minX < $1.minX }
+            .reduce(into: [CGRect]()) { partial, rect in
+                guard let last = partial.last else {
+                    partial.append(rect)
+                    return
+                }
+
+                if rect.minX <= last.maxX + 0.5 {
+                    partial[partial.count - 1] = last.union(rect)
+                } else {
+                    partial.append(rect)
+                }
+            }
+
+        return mergedRects.map {
+            $0.insetBy(
+                dx: 0,
+                dy: -NotionStyle.inlineCodeVerticalPadding
+            )
         }
     }
 }
